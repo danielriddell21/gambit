5941247c0a488f6c7a1b70a44431fe5e0079093e
Dan Riddell <25375432+danielriddell21@users.noreply.github.com>
2026-06-30 16:08:34 +0000

    strip prose from //go:embed doc groups in internal

--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -2,8 +2,5 @@ package assets
 
 import _ "embed"
 
-// Font is the DejaVu Sans TTF, which includes the Unicode chess glyphs
-// (U+2654..U+265F). DejaVu Sans is freely redistributable.
-//
 //go:embed DejaVuSans.ttf
 var Font []byte
